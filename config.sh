a668f00835b524b2bd9d578c0732957e30bcc563
Ivo van Poorten <ivopvp@gmail.com>
2008-06-08 10:58:50 +0000

    split buildtest.sh
    git-svn-id: https://svn.code.sf.net/p/zzjson/code/trunk@13 fca5a30d-054f-0410-8496-fa6b1410ff5f

---
+++ config.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+VERSION=`grep ZZJSON_IDENT zzjson.h | cut -d '"' -f 2 | cut -d ' ' -f 2`
+
+LIBSRC=`eval echo zzjson_{parse,print,query,create,free}.c`
+CC=${CC:-cc}
+LIBS="-lm -lc"
+DEFINES="-D_ISOC99_SOURCE"
+
+if test "`$CC --version 2>&1 | grep -qi gcc && echo gcc`" = gcc ; then
+    WARN="-W -Wall"
+    DEBUG="-g3"
+    OPTMAX="-O3"
+    OPTSIZ="-Os -DCONFIG_NO_ERROR_MESSAGES"
+    OPTMIN="-O0"
+    STRIP="-s"
+elif test "`$CC -flags 2>&1 | grep -qi suncc && echo suncc`" = suncc ; then
+    WARN=""
+    DEBUG="-g"
+    OPTMAX="-xO5"
+    OPTSIZ="-xO5 -xspace -DCONFIG_NO_ERROR_MESSAGES"
+    OPTMIN="-xO1"
+    STRIP="-s1"
+else
+    echo "unknown compiler... aborting" >&2
+    exit 1
+fi
+
+execute() {
+    echo $@
+    $@
+}
