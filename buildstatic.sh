d3648d68228344fedc770c6d630883fe17cad9a1
Ivo van Poorten <ivopvp@gmail.com>
2008-06-08 16:47:46 +0000

    version number in static libs + symlinks
    git-svn-id: https://svn.code.sf.net/p/zzjson/code/trunk@20 fca5a30d-054f-0410-8496-fa6b1410ff5f

--- buildstatic.sh
+++ buildstatic.sh
@@ -3,26 +3,30 @@
 . config.sh
 
 create_lib() {
-    execute rm -f $LIBA
+    execute rm -f $LIBA $LIBAV
     for i in $LIBSRC ; do
         j=${i%%.c}${OBJSUF}
         execute $CC $NOLINK -o $j $CFLAGS $i
-        execute $AR r $LIBA $j
+        execute $AR r $LIBAV $j
     done
+    execute ln -s $LIBAV $LIBA
 }
 
 LIBA=${LIBBASE}$LIBSTATICSUF
+LIBAV=${LIBBASE}.$VERSION$LIBSTATICSUF
 CFLAGS="$OPTMAX $WARN $DEFINES"
 echo "*** static library, optimized"
 create_lib
 
 LIBA=${LIBBASE}_g$LIBSTATICSUF
+LIBAV=${LIBBASE}_g.$VERSION$LIBSTATICSUF
 CFLAGS="$OPTMIN $WARN $DEFINES $DEBUG"
 echo
 echo "*** static library, non-optimized, debug info"
 create_lib
 
 LIBA=${LIBBASE}_s$LIBSTATICSUF
+LIBAV=${LIBBASE}_s.$VERSION$LIBSTATICSUF
 CFLAGS="$OPTSIZ $WARN $DEFINES"
 echo
 echo "*** static library, size optimized"
