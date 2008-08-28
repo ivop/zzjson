b8b7d90279902ab76333b26249d77be5059b2b2f
Ivo van Poorten <ivopvp@gmail.com>
2008-08-28 23:31:18 +0000

    increase micro version number
    git-svn-id: https://svn.code.sf.net/p/zzjson/code/trunk@63 fca5a30d-054f-0410-8496-fa6b1410ff5f

--- include/zzjson.h
+++ include/zzjson.h
@@ -10,9 +10,9 @@
 
 #define ZZJSON_VERSION_MAJOR    1
 #define ZZJSON_VERSION_MINOR    0
-#define ZZJSON_VERSION_MICRO    1
-#define ZZJSON_VERSION_INT      ( 1<<16 | 0<<8 | 1 )
-#define ZZJSON_IDENT            "zzjson 1.0.1"
+#define ZZJSON_VERSION_MICRO    2
+#define ZZJSON_VERSION_INT      ( 1<<16 | 0<<8 | 2 )
+#define ZZJSON_IDENT            "zzjson 1.0.2"
 
 /* Defines: */
 
